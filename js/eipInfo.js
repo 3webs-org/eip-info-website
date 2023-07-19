c948f2a57aab8e04f42e8ed1fb78c1069321e425
Pandapip1 <gavinnjohn@gmail.com>
2023-07-19 14:47:19 -0500

    Remove async lock

--- js/eipInfo.js
+++ js/eipInfo.js
@@ -1,7 +1,6 @@
 import grayMatter from 'gray-matter';
 import yaml from 'js-yaml';
 import Git from 'nodegit';
-import AsyncLock from 'async-lock';
 
 // Generate js-yaml engine that will never throw an error
 
