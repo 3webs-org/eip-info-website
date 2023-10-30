941a21ae8cfbfed70a3be13c9b47ddace772b200
Pandapip1 <gavinnjohn@gmail.com>
2023-10-30 14:06:22 -0500

    Add debug entry

--- .vitepress/eipInfo.js
+++ .vitepress/eipInfo.js
@@ -87,6 +87,8 @@ for (let repoPath of repoPaths) {
 // Sort by date, newest first
 allCommits.sort((a, b) => b[1].date() - a[1].date());
 
+console.dir(allCommits, { depth: null })
+
 // Walk it back
 for (let [repo, commit] of allCommits) {
     try {
